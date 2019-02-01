e1e2f8bbfda617fdc3c513c1dd980ae0c736c601
Alex Hoffman <alxhoff@gmail.com>
2019-02-01 14:55:46 +0100

    more cleanup -_-

--- grapher.py
+++ grapher.py
@@ -2,12 +2,5 @@ from graphviz import Digraph
 
 class grapher:
 
-    def __init__(self):
-        self.graph = Digraph(comment='The Round Table')
-        self.graph.node('A', 'King Arthur')
-        self.graph.node('B', 'Sir Bedevere the Wise')
-        self.graph.node('L', 'Sir Lancelot the Brave')
-        self.graph.edges(['AB', 'AL'])
-        self.graph.edge('B', 'L', constraint='false')
-        self.graph.render('test-output/round-table.gv', view=True)
-
+    def __init__(self, comment):
+        self.comment = comment
