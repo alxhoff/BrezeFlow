a57753b4c7d6e18dc617cf90d1e1e920080b91c9
Alex Hoffman <alxhoff@gmail.com>
2019-11-22 18:27:31 +0100

    Script to compile for all applications and all governors

---
+++ scripts/compile_results.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#script to iterate through each application and each governor,
+#compiling the results as it goes
+
+RESULTS_DIR=$1
+for D in $RESULTS_DIR*/; do
+    if [ -d "${D}" ]; then
+        APP=`basename $D`
+        for dir in $D*/; do
+            if [ -d "${dir}" ]; then
+                GOVERNOR=`basename $dir`
+                COMMAND="python compile_optimizations.py -f $dir -a $APP -g $GOVERNOR"
+                echo $COMMAND
+                eval $COMMAND
+            fi
+        done
+    fi
+done
